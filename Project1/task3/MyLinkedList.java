114e2619fe0ecef41cd943e68c7c76bc32ba1c31
Jonathan wang <65941800+zhi900@users.noreply.github.com>
2021-10-25 04:43:05 -0400

    Update MyLinkedList.java

--- Project1/task3/MyLinkedList.java
+++ Project1/task3/MyLinkedList.java
@@ -1,47 +1,86 @@
 import java.util.Iterator;
 
-// Probably a block class with at least these fields and methods.
-public class MyLinkedList implements Iterable { //generic types are not required, you can just do MyLinkedList for blocks but Iterable is mandatory.
-    //in addition to other regular list member functions such as insert and delete: (split and consolidate blocks must be implemented at the level of linked list)
+
+public class MyLinkedList implements Iterable {
+
     public class Node {
 
-        public int offset;                                  //changed data to offset
+        public int data;
         public int size;
         public Node next;
 
 
-        public Node(int offset, int size, Node next) {      //added Node next as this will add next
-            this.offset = offset;                           //changed data to offset
-            this.size = size;                               
-            this.next = next;                               
+        public Node(int data) {
+            this.data = data;
+            this.size = size;
+            next = null;
         }
+
+        public int getData() {
+            return data;
+        }
+        public void setNext(Node n) {
+            next = n;
+        }
+        public Node getNext() {
+            return next;
+        }
+
     }
-       
-    public Node head = null;                                //added head node decloration
-    
-    public MyLinkedList(Node head) {
-        this.head = head;
-    }
-    
-    //add the sort here or have the list sorted on insertion
-    public void insert(int offset, int size){
+
+    public Node head;
+
+    public void insert(int data){
         if(head == null){
-            head = new Node(offset, size);
+            head = new Node(data);
         } else {
-            Node newNode = new Node(offset, size);
+            Node newNode = new Node(data);
             newNode.setNext(head);
             head = newNode;
+        }
+    }
+
+    public void sortList()
+    {
+
+
+        Node current = head, index = null;
+
+        int temp;
+
+        if (head == null) {
+            return;
+        }
+        else {
+            while (current != null) {
+
+                index = current.next;
+
+                while (index != null) {
+
+
+
+                    if (current.data > index.data) {
+                        temp = current.data;
+                        current.data = index.data;
+                        index.data = temp;
+                    }
+
+                    index = index.next;
+                }
+                current = current.next;
+            }
         }
     }
     public String toString() {
         String result = "";
         Node current = head;
         while(current.getNext() != null){
-            result += current.offset + "-" + current.size;
-            if(current.next != null){
+            result += current.getData();
+            if(current.getNext() != null){
                 result += ", ";
             }
-            current = current.next;
+            current = current.getNext();
         }
         result += current.getData();
         return "Contents of the List: " + result;
@@ -66,46 +105,96 @@ public class MyLinkedList implements Iterable { //generic types are not required
 
     }
     public void insertMayCompact() {
-        
+
+    }
+    public int get(int i) {
+
+        Node u = head;
+        for(int j = 0; j < i; j++){
+            u = u.next;
+        }
+        return u.data;
+
+
+    }
+    public int indexOf(int data) {
+
+        int index = 0;
+        Node current = head;
+
+
+        while(current != null) {
+
+            if(current.data == data) {
+                return index;
+            }
+
+            current = current.next;
+            index++;
+        }
+        return index;
     }
 
 
-    void deleteNode(int position)                           //modified deleteNode method
+    void deleteNode(int position)
     {
-        // If linked list is empty
+
         if (head == null)
             return;
 
-        //memory offset can't be 0
-        if (offset <= 0) {
-            System.err.println("Invalid memory offset: " + offset);
-            return;
-        }
-        
-        //Store head node
+
         Node temp = head;
-        
-        if (offset == 1) {                                  //changes head
+
+
+        if (position == 0)
+        {
             head = temp.next;
             return;
         }
 
-       while (temp != null) {                               //will go throught the list, find temp node where the temp.next.offset==offset 
-           if (temp.next.offset == offset) {                //then chage the parent node to the next next node
-               temp.next = temp.next.next;
-           }
-        temp.next = next;  // Unlink the deleted node from list
-        }
-        System.out.println("Invalid deleteNode did not find node with offset: " + offset);
+
+        for (int i=0; temp!=null && i<position-1; i++)
+            temp = temp.next;
+
+
+        if (temp == null || temp.next == null)
+            return;
+
+
+        Node next = temp.next.next;
+
+        temp.next = next;
     }
 
-    //return total amount in this list, sum of all size
-    public int size(){
-    }
-    
+
+
+
+
     @Override
     public Iterator iterator() {
         return null;
     }
 
+}
+class Block  {
+    int offset;
+    int size;
+
+
+
+    public Block(int offset, int size) {
+        this.offset = offset;
+        this.size = size;
+    }
+
+    public String toString()
+    {
+        return null;
+
+    }
+
+    public boolean is_adjacent(Block other) {
+
+        return false;
+    }
 }
