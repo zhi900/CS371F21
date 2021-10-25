ba4abc8ce1e17bfff82d3f9aed3c4c7b78145f4a
NIlkiv <60949294+NIlkiv@users.noreply.github.com>
2021-10-25 03:21:50 -0400

    Update MyMemoryAllocation.java

--- Project1/task3/MyMemoryAllocation.java
+++ Project1/task3/MyMemoryAllocation.java
@@ -3,71 +3,50 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 // I would like a simulation class to do most of the work.
-public class MyMemoryAllocation extends MyLinkedList {
+public class MyMemoryAllocation extends MemoryAllocation {    //changing extends MyLinkedList to MemoryAllocation
     String algorithm; //best fit, first fit or next fit
     MyLinkedList freeList = new MyLinkedList();
     MyLinkedList usedList = new MyLinkedList();
     int memSize = 0;
     String algo;
     Block block;
-    public MyMemoryAllocation(int mem_size, String algorithm) {
+    public MyMemoryAllocation(int mem_size, String algorithm) {       
         memSize = mem_size;
-        algo = algorithm;
-
-
-        
-        if(freeList.size()!=0) System.out.println(freeList.head.getData());
-        if(usedList.size()!=0)System.out.println(usedList.head.getData());
-        System.out.println(freeList.size());
-
-        
+        this.algorithm = algorithm;                            //changed algo to algorithm.
+        freeList.insert(1, memSize);                           //added freelist insert from 1 to desired range
+        System.out.println("Size: " + this.size());            //added printout to show size of inserted
     }
-
-
-
-    public static void main(String[] args)
+    public static void main(String[] args)                     //moved main up
     {
         MyMemoryAllocation mal = new MyMemoryAllocation(14, "ff");
-
-
-
-
     }
-
-
-
+    
     // Strongly recommend you start with printing out the pieces.
-    public void print() {
+    public void print() {                                      //Removed unnecessary print out
         if(freeList.size()!=0)  System.out.println(freeList.toString());
         if(usedList.size()!=0)  System.out.println(usedList.toString());
     }
 
     public int alloc(int size) {
-        int data = freeList.head.data;
+        int data = freeList.head.data;                         //not sure what is happening here
+                                                               //Are we trying to use best fit or first fit?
+            for (int i = memSize-1;i>0;i--) {
+            freeList.insert(i,i);
 
-            for(int i =0;i<size;i++){
+        }
+      
+        usedList.insert(freeList.head.offset, size);           //changed data to offset as no data is passed but offset is
 
-
-                freeList.setnextHead();
-
-            }
-        usedList.insert(freeList.head.data, size);
-
-
-
-        return data;
-    }
+        return offset;
+    }                                                          //changed data to offset
 
     public void free(int address) {
+        //need to add a call deleteNode(address) on myLinkedList
 
     }
 
 
     public int size() {
-        for (int i =memSize-1;i>0;i--) {
-            freeList.insert(i,i);
-
-        }
         return freeList.size();
     }
 
@@ -76,28 +55,6 @@ public class MyMemoryAllocation extends MyLinkedList {
         return memSize - 1;
     }
 
-}
-class Block  {
-    int offset;
-    int size;
-
-
-
-    public Block(int offset, int size) {
-        this.offset = offset;
-        this.size = size;
-    }
-
-    public String toString() // highly recommended
-    {
-        return null;
-
-    }
-
-    public boolean is_adjacent(Block other) {
-
-        return false;
-    }
 }
 
 // Sort-by in Java: (needs a class)
