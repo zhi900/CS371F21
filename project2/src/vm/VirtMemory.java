c27022a43f22f1e9f4aa2e387f6ecc8e33c6f8c5
NIlkiv <60949294+NIlkiv@users.noreply.github.com>
2021-11-15 23:20:48 -0500

    Update VirtMemory.java

--- project2/src/vm/VirtMemory.java
+++ project2/src/vm/VirtMemory.java
@@ -1,59 +1,99 @@
 package vm;
 
-import org.hamcrest.MatcherAssert;
+import java.util.LinkedList;
+
 import storage.PhyMemory;
-import vm.Memory;
 
+public class VirtMemory extends Memory{
+	int size;
+	int pages;
+	private static final int FRAME_SIZE = 64;
+	MyPageTable pageTable;
+	Policy policy;
+	
+	public VirtMemory() {
+		super(new PhyMemory());
+		size = 64 * 1024;
+		pages = size / FRAME_SIZE;
+		pageTable = new MyPageTable();
+		policy = new Policy(ram.num_frames(), ram);
+		// TODO Auto-generated constructor stub
+	}
 
+	
+	public VirtMemory(PhyMemory ram) {
+		super(ram);
+		size = 64 * 1024;
+		pages = size / FRAME_SIZE;
+		pageTable = new MyPageTable();
+	}
 
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	boolean isValidAddress(int addr) {
+		try {
+			if (addr >= size || addr < 0) {
+				throw new InvalidAddressException();
+			}
+		} catch (InvalidAddressException e) {
+			System.err.println("Invalid address "+ addr);
+			return false;
+		}
+		return true;
+	}
 
-public class VirtMemory extends Memory {
+	@Override
+	public void write(int addr, byte value) {
+		if (!isValidAddress(addr)) {
+			return;
+		}
+		
+		MyPageTable.PageTableEntry entry = loadEntry(addr);
+		ram.write(getFullPhyicalAddress(entry.pfn, addr), value);
+		entry.dirty = true;
+		entry.writesCount += 1;
+		if (entry.writesCount >= 32) {
+			ram.store(entry.vpn, MyPageTable.pfnToAddress(entry.pfn));
+			entry.dirty = false;
+			entry.writesCount = 0;
+		}
+	}
 
-         private static MyPageTable table = new MyPageTable();
-        private int memSize = 64*1024;
-        private int count = 0;
+	MyPageTable.PageTableEntry loadEntry(int addr) {
+		MyPageTable.PageTableEntry entry;
+		// Get the physical page table entry...
+		try {
+			entry = pageTable.get(addr);
+			if (entry.pfn == -1) {
+				policy.setPfn(entry);
+			}
+		} catch (NoPFNException e) {
+			entry = pageTable.put(addr);
+			policy.setPfn(entry);
+		}
+		return entry;
+	}
+	
+	@Override
+	public byte read(int addr) {
+		if (!isValidAddress(addr)) {
+			return 0;
+		}
+		// Get the physical page table entry...
+		MyPageTable.PageTableEntry entry = loadEntry(addr);
+		return ram.read(getFullPhyicalAddress(entry.pfn, addr));
+	}
 
-    public VirtMemory() {
-        super(new PhyMemory(64*1024));
+	@Override
+	protected void sync_to_disk() {
+		// TODO Auto-generated method stub
+		policy.sync_to_disk();
+	}
 
-
-    }
-
-
-
-
-    public void write(int addr, byte value) {
-
-        try {
-
-            if(addr>memSize) {
-                throw new InvalidAddressException();
-            }
-            table.put(addr, value);
-            if(addr>63){
-                count = addr/32;
-                sync_to_disk();
-            }
-
-
-        } catch (InvalidAddressException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-
-    public byte read(int addr) {
-        return (byte) table.getP(addr);
-
-    }
-
-
-    protected void sync_to_disk() {
-            getPhyMemory().store(count,0);
-            getPhyMemory().load(count,0);
-    }
-    }
-
+	public static int getFullPhyicalAddress(int pfn, int logicalAddress) {
+		return (pfn << 6) + ((logicalAddress << 26) >> 26);
+	}
+}
