c7f682e64d62115140d14ff454ba1006b58e2ff4
NIlkiv <60949294+NIlkiv@users.noreply.github.com>
2021-12-19 02:06:05 -0500

    Update BoundedBuffer.java

--- project3/src/utils/BoundedBuffer.java
+++ project3/src/utils/BoundedBuffer.java
@@ -13,18 +13,45 @@ public class BoundedBuffer <T> {
 //implement member functions: deposit() and fetch()
 	//Hashtable<Object, LinkedList<T>> buffer = new Hashtable<Object, LinkedList<T>>();
 	ArrayList<T> buffer = new ArrayList<T>();
-	private Lock mutex = new ReentrantLock(true);
+	private Lock mutex = new ReentrantLock(false);
+	int capacity = 0;
+	int fetchCounter = 0;
+	
+	Condition isFull = mutex.newCondition();
+	Condition isEmpty = mutex.newCondition();
+	
+	public BoundedBuffer(int capacity) {
+		this.capacity = capacity;
+	}
 	
 	public void deposit(T toAdd) {
 			mutex.lock();
+			while (buffer.size() >= capacity) {
+				try {
+					isFull.await();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 			buffer.add(toAdd);
+			isEmpty.signal();
 			mutex.unlock();
 	}
 	
 	public T fetch() {
 		mutex.lock();
+		while(buffer.size() == 0) {
+			try {
+				isEmpty.await();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		T ret = buffer.get(0);
 		buffer.remove(0);
+		isFull.signal();
 		mutex.unlock();
 		return ret;
 	}
