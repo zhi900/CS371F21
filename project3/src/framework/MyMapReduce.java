ea579ecac1ff6ec279ff8d6f640fe1952b8d3242
NIlkiv <60949294+NIlkiv@users.noreply.github.com>
2021-12-20 00:34:21 -0500

    Update MyMapReduce.java

--- project3/src/framework/MyMapReduce.java
+++ project3/src/framework/MyMapReduce.java
@@ -1,7 +1,41 @@
 package framework;
+import java.util.ArrayList;
 import java.util.logging.Level;
 public class MyMapReduce extends MapReduce {
 	//TODO: your code here. Define all attributes
+	
+	class MapThread extends Thread
+	{
+		int threadNumber;
+		String inputFileName;
+		public MapThread(int threadNumber, String inputFileName) {
+			this.threadNumber = threadNumber;
+			this.inputFileName = inputFileName;
+		}
+	    @Override
+	    public void run()
+	    {
+	    	String suffix = threadNumber < 10 ? ".0" + threadNumber : "." + threadNumber;
+			String subFileName = inputFileName + suffix;
+			mapperReducerObj.Map(subFileName);
+	    }
+	}
+	class ReduceThread extends Thread
+	{
+		ArrayList<Object> keys;
+		int partitionNumber;
+		public ReduceThread(int partitionNumber, ArrayList<Object> keys) {
+			this.partitionNumber = partitionNumber;
+			this.keys = keys;
+		}
+	    @Override
+	    public void run()
+	    {
+	    	for(Object key : keys) {
+				mapperReducerObj.Reduce(key, partitionNumber);
+			}
+	    }
+	}
 	
 	ConcurrentKVStore store;
     PartitionTable<Object> partTable;
@@ -19,6 +53,9 @@ public class MyMapReduce extends MapReduce {
 		//TODO: your code here. Delete UnsupportedOperationException after your implementation is done.
 		return partTable.fetchNext(key);
 	}
+	
+	
+	
 	@Override
 	protected void MRRunHelper(String inputFileName,
 		    		  MapperReducerClientAPI mapperReducerObj,
@@ -30,5 +67,34 @@ public class MyMapReduce extends MapReduce {
 		this.num_reducers = num_reducers;
 		partTable = new PartitionTable<Object>(50);
 		this.mapperReducerObj = mapperReducerObj;
+		ArrayList<Thread> threads = new ArrayList<Thread>();
+		for (int i = 0; i < num_mappers; i++) {
+			Thread mapThread = new MapThread(i, inputFileName);
+			threads.add(mapThread);
+			mapThread.start();
+		}
+		for(Thread t : threads) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		threads.clear();
+		ArrayList<ArrayList<Object>> keysInBatches = partTable.GetKeysInBatches(num_reducers, mapperReducerObj);
+		for (int i = 0; i < num_mappers; i++) {
+			Thread reduceThread = new ReduceThread(i, keysInBatches.get(i));
+			threads.add(reduceThread);
+			reduceThread.start();
+		}
+		for(Thread t : threads) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
