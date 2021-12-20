55681ef5ba2631f422a7bf2103fda844aa71182e
NIlkiv <60949294+NIlkiv@users.noreply.github.com>
2021-12-20 15:33:16 -0500

    Update ConcurrentKVStore.java

--- project3/src/framework/ConcurrentKVStore.java
+++ project3/src/framework/ConcurrentKVStore.java
@@ -16,23 +16,36 @@ import utils.BoundedBuffer;
         // store to contain ("foo", {1,1,1}) and ("bar", {1,1}). You can use a
 	// concurrent hashmap/tree to implement the concurrent KV store
 
-public class ConcurrentKVStore {
-	Hashtable<Object, Object> buffer = new Hashtable<Object, Object>();
-	
-	private Lock mutex = new ReentrantLock(true);
-	
-	public void Deposit(Object key, Object toAdd) {
-			//mutex.lock();
-			buffer.put(key, toAdd);
-			//mutex.unlock();
-	}
-	
-	public Object Fetch(Object key) {
-		//mutex.lock();
-		Object ret = buffer.get(key);
-		//mutex.unlock();
-		return ret;
-	}
 
-	
-}
+// Partition table is essentially ConcurrentKVStore combined with BoundedBuffer
+// So a separate ConccurentKVStore is not necessary for this implementation.
+// Our trick is make a BoundedBuffer per key. So it locks per key which should work for dictionary!
+
+//public class ConcurrentKVStore {
+//	Hashtable<Object, LinkedList<Object>> buffer = new Hashtable<Object, LinkedList<Object>>();
+//	
+//	
+//	
+//	public void Deposit(Object key, Object toAdd) {
+//			//mutex.lock();
+//			if (!buffer.contains(key)) {
+//				buffer.put(key, new LinkedList<Object>());
+//			}
+//		
+//			buffer.get(key).add(toAdd);
+//			//mutex.unlock();
+//	}
+//	
+//	public Object Fetch(Object key) {
+//		if (!buffer.contains(key)) {
+//			buffer.put(key, new LinkedList<Object>());
+//		}
+//		//mutex.lock();
+//		if (buffer.get(key).size() == 0) {
+//			return null;
+//		}
+//		Object ret = buffer.get(key).remove();
+//		//mutex.unlock();
+//		return ret;
+//	}	
+//}
